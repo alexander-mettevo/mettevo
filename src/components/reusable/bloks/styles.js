bb4a0725b7b6725be9588679c6e53a8ba733ea7d
Alexander Matvienko <01antizyk.it@gmail.com>
2023-02-03 17:58:18 +0200

    add margin for main bloks

--- src/components/reusable/bloks/styles.js
+++ src/components/reusable/bloks/styles.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const MainBlockWrapper = styled.div`
   margin-top: 280px;
   
-  @media(max-with: 768px) {
+  @media(max-width: 768px) {
     margin-top: 160px;
   }
 `
