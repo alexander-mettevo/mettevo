8a801f656ce23e093780e69ad43227d63ad7bb46
Ваше Имя <you@example.com>
2023-02-07 01:15:25 +0200

    finish info block

--- src/components/pages/services/items/serviseInfo/items/serviceForm/ServiceForm.js
+++ src/components/pages/services/items/serviseInfo/items/serviceForm/ServiceForm.js
@@ -1,10 +1,40 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 const ServiceForm = ({title}) => {
+  const [labelText, setLabelText] = useState(title);
+
+  useEffect(() => {
+    setLabelText(title.replace(/Services/i, 'services'));
+  }, [])
+
   return (
-    <div className={'services-info__form'}>
-      
-    </div>
+    <form className={'services-info__form form-services form'}>
+      <div className={'form-services__block form-services__items-wrap'}>
+        Hello, Mettevo!
+      </div>
+      <div  className={'form-services__items-wrap'}>
+        <label className='input__label form-services__label'>My name is <input className={'form-services__input input'} type='text' placeholder='YOUR NAME'/></label>
+      </div>
+      <div  className={'form-services__items-wrap'}>
+        <label className='input__label form-services__label'>from <input className={'form-services__input input'} type='text' placeholder='COMPANY NAME'/></label>
+      </div>
+      <div  className={'form-services__items-wrap form-services__items-wrap_s'}>
+        <label className='input__label form-services__label form-services__label_w' htmlFor={'service-input'}>I want to get free {labelText} quote of my website</label>
+      </div>
+      <div  className={'form-services__items-wrap'}>
+        <input id={'service-input'} className={'form-services__input input'} type='text' placeholder='website'/>
+      </div>
+      <div  className={'form-services__items-wrap'}>
+        <label className='input__label form-services__label form-services__label_fw'>
+          <span>Contact me back at </span>
+          <input className={'form-services__input input'} type='email' placeholder='YOUR EMAIL'/>
+        </label>
+      </div>
+      <div  className={'form-services__items-wrap'}>
+        <label className='input__label form-services__label'>or <input className={'form-services__input input'} type='text' placeholder='YOUR PHONE'/></label>
+      </div>
+      <button className={'button form-services__button'}>Get a free <span>{title}</span> quote</button>
+    </form>
   );
 };
 
