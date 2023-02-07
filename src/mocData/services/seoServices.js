41b9b9b146950b94d9fb4a9e2b938717cf71dc52
Ваше Имя <you@example.com>
2023-02-07 18:12:57 +0200

    commit

--- src/mocData/services/seoServices.js
+++ src/mocData/services/seoServices.js
@@ -29,7 +29,25 @@ const seoServices = {
         text: 'Some description of the project. Working with ambitious clients gives us the oppor tu nity.'
       },
     ]
-  }
+  },
+  different: [
+    {
+      title: 'Prospect analysis',
+      text: 'Every successful marketing strategy starts with understanding your customer at a granular level. Our SEO team begins by getting to know who you’re targeting.'
+    },
+    {
+      title: 'Competitor Analysis',
+      text: 'Figure out what your competitors are doing so you can position your business and focus your SEO efforts accordingly.'
+    },
+    {
+      title: 'content Building',
+      text: 'Publish content that converts your audience into customers and fans. Build your reputation with high-value information.'
+    },
+    {
+      title: 'On-Page ',
+      text: 'Optimize every on-page element to give your content the best chance of ranking at the top of Google and other search engines.'
+    },
+  ]
 }
 
 export default seoServices;
