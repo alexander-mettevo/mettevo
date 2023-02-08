4f9423e6f101af273bc368cb38172354e7b46991
Alexander Matvienko <01antizyk.it@gmail.com>
2023-02-08 18:23:19 +0200

    commit

--- src/components/pages/services/seo/index/SeoService.js
+++ src/components/pages/services/seo/index/SeoService.js
@@ -3,6 +3,7 @@ import PageHeader from "@/components/pages/services/items/pageHeader/PageHeader"
 import seoServices from "@/mocData/services/seoServices";
 import ServiceInfo from "@/components/pages/services/items/serviseInfo/ServiceInfo";
 import DifferentService from "@/components/pages/services/items/differentService/DifferentService";
+import Changes from "@/components/pages/services/seo/index/items/changes/Changes";
 
 const SeoService = () => {
   return (
@@ -10,6 +11,7 @@ const SeoService = () => {
       <PageHeader url={seoServices.url} title={seoServices.title}/>
       <ServiceInfo title={seoServices.title} info={seoServices.info}/>
       <DifferentService different={seoServices.different}/>
+      <Changes/>
     </>
   );
 };
