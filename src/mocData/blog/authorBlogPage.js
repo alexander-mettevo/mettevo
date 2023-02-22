9b81011c3699b4d12e637465ab23150cf2d54dbd
Alexander Matvienko <01antizyk.it@gmail.com>
2023-02-22 21:23:11 +0200

    author blog page

---
+++ src/mocData/blog/authorBlogPage.js
@@ -0,0 +1,230 @@
+const authorBlogPage = {
+  info: {
+    name: 'Author Name',
+    social: [
+      {
+        title: 'Facebook',
+        href: 'https://www.facebook.com/',
+      },
+      {
+        title: 'Twitter',
+        href: 'https://twitter.com/',
+      },
+      {
+        title: 'LinkedIn',
+        href: 'https://www.linkedin.com/',
+      }
+    ],
+    avatar: {
+      src: '/images/blog/author/Image.png',
+      alt: ''
+    },
+    bio: [
+      'Phasellus id ornare sed nullam eget non facilisi ipsum dolor. Gravida eget aliquam fames consectetur quis. Amet varius diam egestas tincidunt eu massa in. Urna ornare euismod velit amet ut in felis in. Tempor ultricies nulla congue amet dignissim arcu massa ipsum at. Dolor venenatis blandit curabitur vivamus ultrices et bibendum. Ut morbi interdum tincidunt dignissim.',
+      'In elit egestas pulvinar duis turpis ante sem at vel. Vel a nullam mi elementum tortor sit tincidunt neque. Mi interdum mattis cras dui faucibus. Nulla aliquet mollis est est adipiscing est sodales nunc. Elementum scelerisque massa neque purus. Vulputate dignissim tincidunt morbi interdum. Tellus sed ultrices tortor consectetur sit sit elit id urna. Consequat lectus pulvinar quis egestas vitae et quis nunc morbi. Hendrerit lorem consequat non rutrum pretium id purus mauris.',
+      'Vivamus augue quis nunc viverra. Purus nulla pharetra velit odio ante arcu nibh integer aliquam. Integer viverra quam nec tempor sit nulla. Eget nam pellentesque massa ultrices viverra tempus maecenas cras. Sit leo nisl vulputate praesent egestas felis bibendum.'
+    ],
+  },
+  content: [
+    {
+      href: '/',
+      image: {
+        src: '/images/blog/seo/Image.png',
+        size: {
+          width: 807,
+          height: 845
+        },
+        alt: ''
+      },
+      theme: 'theme 1',
+      title: 'Venenatis tincidunt phasellus nec aliquam.',
+      author:{
+        name: 'by Author Name',
+        profile: '/'
+      },
+      date: '2022-12-01T00:00:00.000Z',
+    },
+    {
+      href: '/',
+      image: {
+        src: '/images/blog/seo/Image-1.png',
+        size: {
+          width: 520,
+          height: 640
+        },
+        alt: ''
+      },
+      theme: 'theme 1',
+      title: 'Venenatis tincidunt phasellus nec aliquam.',
+      author:{
+        name: 'by Author Name',
+        profile: '/'
+      },
+      date: '2022-12-01T00:00:00.000Z',
+    },
+    {
+      href: '/',
+      image: {
+        src: '/images/blog/seo/Image-2.png',
+        size: {
+          width: 429,
+          height: 493
+        },
+        alt: ''
+      },
+      theme: 'theme 1',
+      title: 'Venenatis tincidunt phasellus nec aliquam.',
+      author:{
+        name: 'by Author Name',
+        profile: '/'
+      },
+      date: '2022-12-01T00:00:00.000Z',
+    },
+    {
+      href: '/',
+      image: {
+        src: '/images/blog/seo/Image-3.png',
+        size: {
+          width: 453,
+          height: 357
+        },
+        alt: ''
+      },
+      theme: 'theme 1',
+      title: 'Venenatis tincidunt phasellus nec aliquam.',
+      author:{
+        name: 'by Author Name',
+        profile: '/'
+      },
+      date: '2022-12-01T00:00:00.000Z',
+    },
+    {
+      href: '/',
+      image: {
+        src: '/images/blog/seo/Image-4.png',
+        size: {
+          width: 379,
+          height: 426
+        },
+        alt: ''
+      },
+      theme: 'theme 1',
+      title: 'Venenatis tincidunt phasellus nec aliquam.',
+      author:{
+        name: 'by Author Name',
+        profile: '/'
+      },
+      date: '2022-12-01T00:00:00.000Z',
+    },
+    {
+      href: '/',
+      image: {
+        src: '/images/blog/seo/Image-5.png',
+        size: {
+          width: 651,
+          height: 575
+        },
+        alt: ''
+      },
+      theme: 'theme 1',
+      title: 'Venenatis tincidunt phasellus nec aliquam.',
+      author:{
+        name: 'by Author Name',
+        profile: '/'
+      },
+      date: '2022-12-01T00:00:00.000Z',
+    },
+    {
+      href: '/',
+      image: {
+        src: '/images/blog/seo/Image-6.png',
+        size: {
+          width: 713,
+          height: 498
+        },
+        alt: ''
+      },
+      theme: 'theme 1',
+      title: 'Venenatis tincidunt phasellus nec aliquam.',
+      author:{
+        name: 'by Author Name',
+        profile: '/'
+      },
+      date: '2022-12-01T00:00:00.000Z',
+    },
+    {
+      href: '/',
+      image: {
+        src: '/images/blog/seo/Image-7.png',
+        size: {
+          width: 553,
+          height: 456
+        },
+        alt: ''
+      },
+      theme: 'theme 1',
+      title: 'Venenatis tincidunt phasellus nec aliquam.',
+      author:{
+        name: 'by Author Name',
+        profile: '/'
+      },
+      date: '2022-12-01T00:00:00.000Z',
+    },
+    {
+      href: '/',
+      image: {
+        src: '/images/blog/seo/Image-8.png',
+        size: {
+          width: 263,
+          height: 380
+        },
+        alt: ''
+      },
+      theme: 'theme 1',
+      title: 'Venenatis tincidunt phasellus nec aliquam.',
+      author:{
+        name: 'by Author Name',
+        profile: '/'
+      },
+      date: '2022-12-01T00:00:00.000Z',
+    },
+    {
+      href: '/',
+      image: {
+        src: '/images/blog/seo/Image-9.png',
+        size: {
+          width: 497,
+          height: 438
+        },
+        alt: ''
+      },
+      theme: 'theme 1',
+      title: 'Venenatis tincidunt phasellus nec aliquam.',
+      author:{
+        name: 'by Author Name',
+        profile: '/'
+      },
+      date: '2022-12-01T00:00:00.000Z',
+    },
+    {
+      href: '/',
+      image: {
+        src: '/images/blog/seo/Image-9.png',
+        size: {
+          width: 497,
+          height: 438
+        },
+        alt: ''
+      },
+      theme: 'theme 1',
+      title: 'Venenatis tincidunt phasellus nec aliquam.',
+      author:{
+        name: 'by Author Name',
+        profile: '/'
+      },
+      date: '2022-12-01T00:00:00.000Z',
+    },
+  ]
+}
+
+export default authorBlogPage
